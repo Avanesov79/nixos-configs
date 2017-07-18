f7855e7053ba725cb3f32b8712eae6527958d7ae
Avanesov79 <avanesov@gmail.com>
2017-07-18 17:32:02 -0500

    Added gui related cosmetic pkgs and a few other items

--- de.i3-gaps.nix
+++ de.i3-gaps.nix
@@ -15,35 +15,68 @@
   # Set packages to be installed
   environment.systemPackages = with pkgs; [
 ##### Packages specific to the i3 desktop setup I want #####
-    i3lock
-    i3blocks-gaps
-    i3status
-    nitrogen
-    lxqt.pcmanfm-qt
-    kdeFrameworks.oxygen-icons5
-    pavucontrol
-    networkmanager
-    irssi
-    rxvt_unicode
-    mpv
-    moc
-    pulseaudioFull
-    playerctl
-    arandr
-    font-awesome-ttf
-    lxappearance
-    arc-theme
-    arc-icon-theme
-    plasma-workspace-wallpapers
-    xorg.xcursorthemes
-    moka-icon-theme
-    rofi
-    rofi-menugen
-    compton
-    lxqt.compton-conf
-##### Packages I want on every GUI desktop system #####
-    chromium
-    handbrake
+############################################################
+##### Programs #####
+    chromium                            # GUI Webbrowser
+    handbrake                           # DVD ripper
+    gimp                                # Image manipulation and drawing program
+    gwenview                            # Image viewer
+    nitrogen                            # wallpaper selector and restorer
+    irssi                               # IRC client
+    mpv                                 # Video player
+    moc                                 # Audio player
+    rxvt_unicode                        # Preferred terminal emulator
+##### System Utilities #####
+    i3lock                              # Screen lock program
+    i3blocks-gaps                       # i3 wm with gaps support
+    #i3status                           # Status info for i3-bar (using i3blocks instead)
+    compton                             # Compositor for use with i3
+    rofi                                # Program runner/launcher
+    rofi-menugen                        # Menu generator for rofi
+    lxqt.compton-conf                   # GUI config tool for compton
+    lxqt.pcmanfm-qt                     # qt based file manager
+    pavucontrol                         # Volume control for pulse audio
+    networkmanager                      # network manager- use nmtui for ncurses interface
+    pulseaudioFull                      # Pulse audio sound server
+    playerctl                           # Allows binding keys to commands for media player control
+    arandr                              # GUI frontend to xrandr for multi-monitor configuration
+    lxappearance                        # theme configuration for gtk2 and gtk3 window decoration
+##### Icons, Themes, and Wallpapers #####
+    ## Fonts
+    dejavu_fonts
+    freefont_ttf
+    google-fonts
+    nerdfonts
+    font-awesome-ttf                    # Fonts
+    oxygenfonts
+    ## Theme Pack (window decorations)
+    arc-theme                           # gtk theme
+    gnome-breeze                        # gtk theme to match kde breeze
+    adapta-gtk-theme
+    flat-plat
+    numix-gtk-theme
+    paper-gtk-theme
+    ## Wallpapers
+    plasma-workspace-wallpapers         # Wallpaper pack
+    ## Cursor themes
+    xorg.xcursorthemes                  # cursor theme
+    ## Icon Themes
+    arc-icon-theme                      # Icon theme
+    kdeFrameworks.oxygen-icons5         # Icon theme
+    hicolor-icon-theme                  # Icon theme
+    moka-icon-theme                     # Icon theme
+    xfce.xfce4icontheme                 # Icon Theme
+    elementary-icon-theme               # Icon theme
+    faba-icon-theme
+    gnome2.gnome_icon_theme
+    maia-icon-theme
+    mate.mate-icon-theme
+    mate.mate-icon-theme-faenza
+    numix-icon-theme
+    numix-icon-theme-circle
+    numix-icon-theme-square
+    paper-icon-theme
+    tango-icon-theme
     ];
   ##### List services that you want to enable:
   hardware.pulseaudio.enable = true;
